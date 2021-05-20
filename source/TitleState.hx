c24d6f23b67e4535a622fa201cc616b5eaf64e0c
CliveDev0413 <anoodleofcode@gmail.com>
2021-05-20 15:49:25 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -371,7 +371,7 @@ class TitleState extends MusicBeatState
 
 		switch (curBeat)
 		{
-			case 4:
+			case 3:
 				createCoolText(['Verwex,   Kadedev', 'Ash237', 'present']);
 			// credTextShit.text += '\npresent...';
 			// credTextShit.addText();
